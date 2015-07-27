bb7e591178f072dfefcb79569382ec20d399ffce
kilbergr <rekilberg@gmail.com>
2015-07-27 11:02:35 -0700

    routes

--- twixtApp/config/routes.rb
+++ twixtApp/config/routes.rb
@@ -1,88 +1,47 @@
 Rails.application.routes.draw do
-  get 'items/index'
 
-  get 'items/new'
+  root 'users#show'
+  resources 'users'
 
-  get 'items/edit'
+  resources 'groups' do
+    resources 'lists', shallow: true do
+      resources 'items', shallow: true 
+    end
+  end
+  
 
-  get 'items/show'
-
-  get 'lists/index'
-
-  get 'lists/new'
-
-  get 'lists/edit'
-
-  get 'lists/show'
-
-  get 'groups/index'
-
-  get 'groups/new'
-
-  get 'groups/edit'
-
-  get 'groups/show'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/show'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+      # Prefix Verb   URI Pattern                           Controller#Action
+#           root GET    /                                     users#show
+#          users GET    /users(.:format)                      users#index
+#                POST   /users(.:format)                      users#create
+#       new_user GET    /users/new(.:format)                  users#new
+#      edit_user GET    /users/:id/edit(.:format)             users#edit
+#           user GET    /users/:id(.:format)                  users#show
+#                PATCH  /users/:id(.:format)                  users#update
+#                PUT    /users/:id(.:format)                  users#update
+#                DELETE /users/:id(.:format)                  users#destroy
+#     list_items GET    /lists/:list_id/items(.:format)       items#index
+#                POST   /lists/:list_id/items(.:format)       items#create
+#  new_list_item GET    /lists/:list_id/items/new(.:format)   items#new
+#      edit_item GET    /items/:id/edit(.:format)             items#edit
+#           item GET    /items/:id(.:format)                  items#show
+#                PATCH  /items/:id(.:format)                  items#update
+#                PUT    /items/:id(.:format)                  items#update
+#                DELETE /items/:id(.:format)                  items#destroy
+#    group_lists GET    /groups/:group_id/lists(.:format)     lists#index
+#                POST   /groups/:group_id/lists(.:format)     lists#create
+# new_group_list GET    /groups/:group_id/lists/new(.:format) lists#new
+#      edit_list GET    /lists/:id/edit(.:format)             lists#edit
+#           list GET    /lists/:id(.:format)                  lists#show
+#                PATCH  /lists/:id(.:format)                  lists#update
+#                PUT    /lists/:id(.:format)                  lists#update
+#                DELETE /lists/:id(.:format)                  lists#destroy
+#         groups GET    /groups(.:format)                     groups#index
+#                POST   /groups(.:format)                     groups#create
+#      new_group GET    /groups/new(.:format)                 groups#new
+#     edit_group GET    /groups/:id/edit(.:format)            groups#edit
+#          group GET    /groups/:id(.:format)                 groups#show
+#                PATCH  /groups/:id(.:format)                 groups#update
+#                PUT    /groups/:id(.:format)                 groups#update
+#                DELETE /groups/:id(.:format)                 groups#destroy
 end
