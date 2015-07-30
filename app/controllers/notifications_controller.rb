8f25929d05ee258c0534321418c4950adb8cbfe0
415DomSmith <415.dominic.smith@gmail.com>
2015-07-30 11:37:45 -0700

    moved text msg form to jquery

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,7 @@ class NotificationsController < ApplicationController
 	before_action :find_item
 
 	def create
+		binding.pry
 		#EXTRACT DATA FROM PARAMS TO FORMAT BEFORE IT GOES IN TO DB (format phone number and figure out how to turn send_by in to real numbers)
 		@notification = Notification.new(notification_params)
     @notification.save
